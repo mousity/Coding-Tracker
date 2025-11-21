69533185fc433b03499f7dfef5026dbb8a25e47b
Mousity <samr.mouna@gmail.com>
2025-11-20 22:43:31 -0500

    Added NuGet packages

--- coding-tracker/Program.cs
+++ coding-tracker/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using Microsoft.Data.Sqlite;
+
+namespace coding_tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=coding_tracker.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+            }
+        }
+    }
+}
